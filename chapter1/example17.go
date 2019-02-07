b9af3c1f0ad0947948dcf0a35703a0cbaf9df289
Mauricio Rojas <mrojasb2000@gmail.com>
2019-02-07 14:42:10 -0300

    Structures - collection of multiple data types as a single entity.

---
+++ chapter1/example17.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type student struct {
+	rollNo int
+	name   string
+}
+
+func main() {
+	stud := student{1, "johnny"}
+	fmt.Println("Student name ::", stud.name)
+	pstud := &stud
+	fmt.Println("Student name ::", pstud.name)
+	fmt.Println(student{rollNo: 2, name: "Ann"})
+	fmt.Println(student{name: "Ann", rollNo: 2})
+	fmt.Println(student{name: "Alice"})
+}
