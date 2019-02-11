3b24ffdee3552904a5cc9ad44659c64f37134294
Mauricio Rojas <mrojasb2000@gmail.com>
2019-02-11 15:39:45 -0300

    Array.

---
+++ chapter1/example21.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var arr [10]int
+	fmt.Println(arr)
+	for i := 0; i < 10; i++ {
+		arr[i] = i
+	}
+	fmt.Println(arr)
+	count := len(arr)
+	fmt.Println("Length fo array:", count)
+}
