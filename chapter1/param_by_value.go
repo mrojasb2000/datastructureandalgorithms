68b1413728ff38fb95db3ad7f33c8ceb62c6a008
Mauricio Rojas <mrojasb2000@gmail.com>
2019-06-04 17:26:05 -0400

    Refactor parameter passsing call by value.

--- chapter1/param_by_value.go
+++ chapter1/param_by_value.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 )
 
+// IncrementPassByValue test pass parameters
+func IncrementPassByValue(x int) {
+	fmt.Println("Incrment value...")
+	x++
+}
+
 func main() {
 	i := 10
 	fmt.Println("Value of i before increment is:", i)
 	IncrementPassByValue(i)
 	fmt.Println("Value of i after increment is:", i)
-}
-
-func IncrementPassByValue(x int) {
-	x++
 }
