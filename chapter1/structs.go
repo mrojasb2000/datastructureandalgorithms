91b2b0de0d1cae6c0eb6159755e822e04b65fc0b
Mauricio Rojas <mrojasb2000@gmail.com>
2019-06-06 14:08:15 -0400

    Comment initialization struct.

--- chapter1/structs.go
+++ chapter1/structs.go
@@ -12,10 +12,15 @@ type student struct {
 func main() {
 	stud := student{1, "johnny"}
 	fmt.Println(stud)
+	// Accessing inner fields
 	fmt.Println("Student name ::", stud.name)
 	pstud := &stud
+	// Accessing inner fields
 	fmt.Println("Student name ::", pstud.name)
+	// Named initialization
 	fmt.Println(student{rollNo: 2, name: "Ann"})
+	// Order does not matter
 	fmt.Println(student{name: "Ann", rollNo: 2})
+	// Default initialization of rollNo
 	fmt.Println(student{name: "Alice"})
 }
