c6ec35093e47e00b4a4388ce155c622c047add3b
Mauricio Rojas <mrojasb2000@gmail.com>
2020-12-30 20:14:59 -0300

    Added List doubly linked list

---
+++ chapter01/list.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	var intlist list.List
+	intlist.PushBack(11)
+	intlist.PushBack(23)
+	intlist.PushBack(34)
+
+	for element := intlist.Front(); element != nil; element = element.Next() {
+		fmt.Println(element.Value.(int))
+	}
+}
