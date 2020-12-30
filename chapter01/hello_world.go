f26d4f97f6977da8001f484ea37951afa80018a7
Mauricio Rojas <mrojasb2000@gmail.com>
2020-12-30 16:48:26 -0300

    Added hello world program

---
+++ chapter01/hello_world.go
@@ -0,0 +1,11 @@
+//main package has examples shown
+//in Hands-On Data Structures and algorithms with Go book
+package main
+
+// importing fmt package
+import "fmt"
+
+// main method
+func main() {
+	fmt.Println("Hello World")
+}
