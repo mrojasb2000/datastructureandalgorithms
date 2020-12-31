add33c4b8f345cfa515abe81b9fbc471bdb6020b
Mauricio Rojas <mrojasb2000@gmail.com>
2020-12-31 10:06:56 -0300

    Refactor fifo and lifo list

--- chapter01/list.go
+++ chapter01/list.go
@@ -7,9 +7,25 @@ import (
 
 func main() {
 	var intlist list.List
+
+	fmt.Println("FIFO")
+	// FIFO
 	intlist.PushBack(11)
 	intlist.PushBack(23)
 	intlist.PushBack(34)
+
+	for element := intlist.Front(); element != nil; element = element.Next() {
+		fmt.Println(element.Value.(int))
+	}
+
+	// Clean list
+	intlist.Init()
+	fmt.Println("LIFO")
+
+	// LIFO
+	intlist.PushFront(11)
+	intlist.PushFront(23)
+	intlist.PushFront(34)
 
 	for element := intlist.Front(); element != nil; element = element.Next() {
 		fmt.Println(element.Value.(int))
